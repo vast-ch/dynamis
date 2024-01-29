44257da637afeea87de97f443ecc136b90f0cb33
Michal Bryxí <michal.bryxi@gmail.com>
2024-01-29 19:14:19 +0100

    chore: Linters kicking in
    - No idea why now.

--- app/app.ts
+++ app/app.ts
@@ -3,7 +3,7 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'energaudi/config/environment';
 
-import './app.css'
+import './app.css';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
