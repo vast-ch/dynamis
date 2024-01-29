8192695bb97642718c9169f4112035696508f9f1
Michal Bryxí <michal.bryxi@gmail.com>
2024-01-29 22:51:01 +0100

    feat: Another set of sections.

---
+++ app/components/main/hero/phone.ts
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import phone from '/assets/images/phone.png?lqip=inline&widths=316&responsive';
+
+export default class MainHeroPhoneComponent extends Component {
+  phone = phone;
+}
