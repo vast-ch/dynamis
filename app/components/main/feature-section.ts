c6af08a3486bfb2a45b2c238bcd699d03e9af7c4
Michal Bryxí <michal.bryxi@gmail.com>
2024-01-30 08:38:20 +0100

    feat: Some new sections on main page

---
+++ app/components/main/feature-section.ts
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import laptop from '/assets/images/laptop.png?lqip=inline&widths=2432&responsive';
+
+export default class MainHeroFeatureSectionComponent extends Component {
+  laptop = laptop;
+}
