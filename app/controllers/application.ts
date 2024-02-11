b45b95eefb6ecfb9abb7d87d05b5b4bf22d982fa
Michal Bryxí <michal.bryxi@gmail.com>
2024-02-11 14:38:45 +0100

    fix: Cross linking buttons do correct scroll-to-top

--- app/controllers/application.ts
+++ app/controllers/application.ts
@@ -1,8 +1,11 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
+import type ScrollService from 'energaudi/services/scroll';
 
 export default class ApplicationController extends Controller {
   queryParams = ['scrollTo'];
 
+  @service declare scroll: ScrollService;
   @tracked scrollTo = null;
 }
